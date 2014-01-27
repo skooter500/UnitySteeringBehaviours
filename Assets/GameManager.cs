772a2e2fe78046059b21a1a3cd37f65dd13c3b38
Bryan Duggan <skooter500@gmail.com>
2014-01-27 19:44:50 +0000

    Bla

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 
 public class GameManager : MonoBehaviour {
+
+    StringBuilder message = new StringBuilder();
 
     static GameManager instance;
 	// Use this for initialization
@@ -20,9 +24,22 @@ public class GameManager : MonoBehaviour {
         return instance;
     }
 
+    void OnGUI()
+    {
+        string msg = new string(message.ToString().ToCharArray());
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), msg);
+        Debug.Log("On GUI called: " + msg);
+        //message.Length = 0;
+    }
+
+    public void AddMessage(string message)
+    {
+        this.message.Append(message + "\n");
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Debug.Log("Update called");
 	}
 }
---
+++ Assets/Perception.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Perception : MonoBehaviour {
+    GameObject otherShip;
+
+	// Use this for initialization
+	void Start () {
+        otherShip = GameObject.FindGameObjectWithTag("ferdelance");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 toOtherShip = otherShip.transform.position - gameObject.transform.position;
+        string message;
+        if (toOtherShip.magnitude > 20.0f)
+        {
+            message = "Out of range";            
+        }
+        else
+        {
+            message = "In range";
+        }
+        GameManager.Instance().AddMessage("In range");
+        Debug.Log("" + message);
+
+        toOtherShip.Normalize();
+
+        float dot = Vector3.Dot(toOtherShip, transform.forward);
+        if (dot < 0)
+        {
+            GameManager.Instance().AddMessage("Behind");
+        }
+        else
+        {
+            GameManager.Instance().AddMessage("In front");
+        }
+
+	}
+}
